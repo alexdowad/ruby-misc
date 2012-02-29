37fb2be7346e0bd81505fdcdbbe59497889b650d
Alex Dowad <alexinbeijing@gmail.com>
2012-02-29 22:23:12 +0200

    Trying ideas from Alex K

--- ruby-threads/read_write_lock.rb
+++ ruby-threads/read_write_lock.rb
@@ -38,17 +38,10 @@ require 'thread'
 
 class ReadWriteLock
   def initialize
-    @counter      = Atomic.new(0)         # single integer which represents lock state
-    @reader_q     = ConditionVariable.new # queue for waiting readers
-    @reader_mutex = Mutex.new             # to protect reader queue
-    @writer_q     = ConditionVariable.new # queue for waiting writers
-    @writer_mutex = Mutex.new             # to protect writer queue
+    @counter = Atomic.new(0)         # single integer which represents lock state
+    @queue   = ConditionVariable.new # queue for waiting threads
+    @mutex   = Mutex.new             # to protect queue
   end
-
-  WAITING_WRITER  = 1 << 15
-  RUNNING_WRITER  = 1 << 30
-  MAX_READERS     = WAITING_WRITER - 1
-  MAX_WRITERS     = RUNNING_WRITER - MAX_READERS - 1
 
   def with_read_lock
     acquire_read_lock
@@ -64,78 +57,90 @@ class ReadWriteLock
     result
   end
 
-  def acquire_read_lock
-    while(true)
-      c = @counter.value
-      raise "Too many reader threads!" if (c & MAX_READERS) == MAX_READERS
+  WAITING_WRITER  = 1 << 15
+  RUNNING_WRITER  = 1 << 30
+  MAX_READERS     = WAITING_WRITER - 1
+  MAX_WRITERS     = RUNNING_WRITER - MAX_READERS - 1
 
-      # If a writer is waiting when we first queue up, we need to wait
-      if c >= WAITING_WRITER
-        # But it is possible that the writer could finish and decrement @counter right here...
-        @reader_mutex.synchronize do 
-          # So check again inside the synchronized section
-          @reader_q.wait(@reader_mutex) if @counter.value >= WAITING_WRITER
-        end
+  private
+  def reader_running?
+    (@counter.value & MAX_READERS) > 0
+  end
+  def too_many_readers?
+    (@counter.value & MAX_READERS) == MAX_READERS
+  end
 
-        # after a reader has waited once, they are allowed to "barge" ahead of waiting writers
-        # but if a writer is *running*, the reader still needs to wait (naturally)
-        while(true)
-          c = @counter.value
-          if c >= RUNNING_WRITER
-            @reader_mutex.synchronize do
-              @reader_q.wait(@reader_mutex) if @counter.value >= RUNNING_WRITER
-            end
-          else
-            return if @counter.compare_and_swap(c,c+1)
-          end
-        end
-      else
-        break if @counter.compare_and_swap(c,c+1)
+  def writer_running?
+    (@counter.value & RUNNING_WRITER) != 0
+  end
+  def writer_waiting?
+    (@counter.value & MAX_WRITERS) != 0
+  end
+  def writer_present?
+    @counter.value >= WAITING_WRITER
+  end
+  def too_many_writers?
+    (@counter.value & MAX_WRITERS) == MAX_WRITERS
+  end
+
+  def thread_running?
+    reader_running? || writer_running?
+  end
+
+  def sleep_if
+    if slept = yield
+      @mutex.synchronize do
+        @queue.wait(@mutex) if slept = yield
       end
-    end    
+    end
+    slept
+  end
+  def wake_one
+    @mutex.synchronize do
+      @queue.signal
+    end
+  end
+  def with_counter
+    while(true)
+      yield @counter.value
+    end
+  end
+  def swap(old,new)
+    @counter.compare_and_swap(old,new)
+  end
+
+  def acquire_read_lock
+    with_counter do |c|
+      raise "Too many reader threads!" if too_many_readers?
+      if sleep_if { writer_present? }
+        wake_one # so that all readers at front of queue will be woken up...
+      else
+        break if swap(c,c+1)
+      end
+    end
   end
 
   def release_read_lock
-    while(true)
-      c = @counter.value
-      if @counter.compare_and_swap(c,c-1)
-        # If one or more writers were waiting, and we were the last reader, wake a writer up
-        if c >= WAITING_WRITER && (c & MAX_READERS) == 1
-          @writer_mutex.synchronize { @writer_q.signal }
-        end
+    with_counter do |c|
+      if swap(c,c-1)
+        # If one or more writers are waiting, and we were the last reader, wake a writer up
+        wake_one if writer_waiting? && ((c & MAX_READERS) == 1)
         break
       end
     end
   end
 
   def acquire_write_lock
-    while(true)
-      c = @counter.value
-      raise "Too many writers!" if (c & MAX_WRITERS) == MAX_WRITERS
+    with_counter do |c|
+      raise "Too many writers!" if too_many_writers?
 
-      if c == 0 # no readers OR writers running
-        # if we successfully swap the RUNNING_WRITER bit on, then we can go ahead
-        break if @counter.compare_and_swap(0,RUNNING_WRITER)
-      elsif @counter.compare_and_swap(c,c+WAITING_WRITER)
-        while(true)
-          # Now we have successfully incremented, so no more readers will be able to increment
-          #   (they will wait instead)
-          # However, readers OR writers could decrement right here, OR another writer could increment
-          @writer_mutex.synchronize do
-            # So we have to do another check inside the synchronized section
-            # If a writer OR reader is running, then go to sleep
-            c = @counter.value
-            @writer_q.wait(@writer_mutex) if (c >= RUNNING_WRITER) || ((c & MAX_READERS) > 0)
+      if not thread_running?
+        break if swap(0,RUNNING_WRITER)
+      elsif swap(c,c+WAITING_WRITER)
+        with_counter do |c|
+          unless sleep_if { thread_running? }
+            break if swap(c,c+RUNNING_WRITER-WAITING_WRITER)
           end
-
-          # We just came out of a wait
-          # If we successfully turn the RUNNING_WRITER bit on with an atomic swap,
-          # Then we are OK to stop waiting and go ahead
-          # Otherwise go back and wait again
-          c = @counter.value
-          break if (c < RUNNING_WRITER) && 
-                   ((c & MAX_READERS) == 0) &&
-                   @counter.compare_and_swap(c,c+RUNNING_WRITER-WAITING_WRITER)
         end
         break
       end
@@ -143,13 +148,9 @@ class ReadWriteLock
   end
 
   def release_write_lock
-    while(true)
-      c = @counter.value
-      if @counter.compare_and_swap(c,c-RUNNING_WRITER)
-        @reader_mutex.synchronize { @reader_q.broadcast }
-        if (c & MAX_WRITERS) > 0 # if any writers are waiting...
-          @writer_mutex.synchronize { @writer_q.signal }
-        end
+    with_counter do |c|
+      if swap(c,c-RUNNING_WRITER)
+        wake_one
         break
       end
     end
@@ -173,7 +174,7 @@ if __FILE__ == $0
 
 require 'benchmark'
 
-TOTAL_THREADS = 100 # set as high as practicable, for a better test
+TOTAL_THREADS = 10 # set as high as practicable, for a better test
 
 # if -i command line option is used, print a 'r' or 'w' each time a reader or writer
 #   runs, so we can see whether either readers or writers are being starved
