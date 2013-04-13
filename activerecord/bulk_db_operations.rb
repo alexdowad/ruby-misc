8df9a89b8e39f54c31c594f3d603c2b5d77852b9
Alex Dowad <alexinbeijing@gmail.com>
2013-04-13 19:17:53 +0200

    #bulk_insert now saves manually set primary key values

--- activerecord/bulk_db_operations.rb
+++ activerecord/bulk_db_operations.rb
@@ -9,6 +9,7 @@ Arel::Visitors::ToSql.class_eval do
 
   # Arel's implementation of INSERT statements ONLY handles the case where a single record is being inserted
   # We'll extend it to handle multiple records
+  #
   # I don't want to mess around with the internals of the Arel::Nodes::InsertStatement class,
   #   because it's used in a lot of places and we could easily break something
   # Instead, we'll say that if the number of values for an InsertStatement is 2x or more the number
@@ -51,23 +52,32 @@ module Enumerable
     # Chew out callers who give us invalid input
     raise "No, no! You are very bad!" unless all?(&:new_record?) && all? { |record| record.class == first.class }
 
-    values  = []
-    columns = nil
-    each do |record|
-      attrs = record.send(:arel_attributes_values, false).to_a
-      attrs.sort_by! { |k,_| k.name }
-      columns = attrs.map(&:first) if columns.nil?
-      values.concat(attrs.map { |_,v| v })
+    pk = first.class.primary_key
+    with_pk,without_pk = self.partition { |record| record[pk] }
+    table = first.class.arel_table
+
+    do_bulk_insert = lambda do |records, insert_pk|
+      values  = []
+      columns = nil
+      records.each do |record|
+        attrs = record.send(:arel_attributes_values, insert_pk).to_a
+        attrs.sort_by! { |k,_| k.name }
+        columns = attrs.map(&:first) if columns.nil?
+        values.concat(attrs.map { |_,v| v })
+      end
+
+      insert = Arel::InsertManager.new(ActiveRecord::Base)
+      insert.into(table)
+      insert.columns = columns
+      insert.values = insert.create_values(values, columns)
+      insert.ast.relation = columns.first.relation
+      ActiveRecord::Base.connection.execute(insert.to_sql)
+
+      records.each { |r| r.instance_variable_set(:@new_record, false) }
     end
 
-    table = first.class.arel_table
-    insert = Arel::InsertManager.new(ActiveRecord::Base)
-    insert.into(table)
-    insert.columns = columns
-    insert.values = insert.create_values(values, columns)
-    insert.ast.relation = columns.first.relation
-
-    ActiveRecord::Base.connection.execute(insert.to_sql)
+    do_bulk_insert[with_pk, true] unless with_pk.empty?
+    do_bulk_insert[without_pk, false] unless without_pk.empty?
     true
   end
 end
